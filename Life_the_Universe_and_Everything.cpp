2b7a2bb9ecfc5324b027c488ff42d2fc174e3cd6
shrujansonawane <87773696+shrujansonawane@users.noreply.github.com>
2022-01-04 08:31:47 +0530

    Create Life_the_Universe_and_Everything.cpp

---
+++ Life_the_Universe_and_Everything.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    int num;
+
+    for(;;){
+
+    cin>> num;
+
+    if(num!=42)
+
+    {
+        cout<<num<<endl;    
+
+    }
+    else
+
+    {
+        exit(0);
+
+    }
+
+    }
+
+}
+
