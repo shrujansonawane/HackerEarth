279cd69a982160c3bf3c7116a3aaa4c41a8c9cc9
Shrujan Sonawane <87773696+shrujansonawane@users.noreply.github.com>
2022-01-27 22:58:24 +0530

    Create Pile_of_Coins.cpp

---
+++ Pile_of_Coins.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int num;
+
+    cin >> num;         
+
+    while(num--)
+
+    {
+
+        int m,n;
+
+        cin>>m>>n;
+
+        if(m%2==0||n%2==0) cout<<"Ashish"<<endl;
+
+        else cout<<"Jeel"<<endl;
+
+    }
+
+}
